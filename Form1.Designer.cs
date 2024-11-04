d6a224decb6cfd7aa8cd7d8c884213c4ab79fd6a
PedroVictorAndreoli <114033109+PedroVictorAndreoli@users.noreply.github.com>
2024-11-04 10:54:56 -0300

    Update Vet Pacotes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -542,6 +542,7 @@
             button5.TabIndex = 58;
             button5.Text = "Import";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // button6
             // 
